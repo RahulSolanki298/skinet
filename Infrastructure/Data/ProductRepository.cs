0ce9b4c9587114855fe5372f8eb49afbd49001ad
Rahul Solanki <rahulsolanki259@gmail.com>
2023-02-09 05:36:26 +0530

    Add SpecificationPattern

--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -14,12 +14,18 @@ namespace Infrastructure.Data
 
         public async Task<Product> GetProductByIdAsync(int id)
         {
-            return await _context.Products.FirstOrDefaultAsync(x=>x.Id == id);
+            return await _context.Products
+                                 .Include(p=>p.ProductBrand)
+                                 .Include(p=>p.ProductType)
+                                 .FirstOrDefaultAsync(x=>x.Id == id);   
         }
 
         public async Task<IReadOnlyList<Product>> GetProductsAsync()
         {
-            return await _context.Products.ToListAsync();
+            return await _context.Products
+                                 .Include(p=>p.ProductBrand)
+                                 .Include(p=>p.ProductType)
+                                 .ToListAsync();
         }
 
         public async Task<IReadOnlyList<ProductBrand>> GetBrandsAsync()
